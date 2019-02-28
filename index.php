17c1dbfcdcef8b7ca72df7f4bbf01a02871fc9d8
david john d. morandarte <morandartedj123@gmail.com>
2019-02-28 08:54:11 +0800

    FINAL Push Index
    HAPPY GRADUATION !!

--- index.php
+++ index.php
@@ -79,7 +79,7 @@
         
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark-green fixed-top">
         <div class="container">
-          <a class="navbar-brand" href="#">BUPC MAP INTERACTIVE</a>
+          <a class="navbar-brand" href="#">BUPC INTERACTIVE MAP</a>
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
